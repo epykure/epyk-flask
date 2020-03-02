28ce5cbcddaa18e2d1f8b7b2ee2d3ea7fa24eac2
epykachu <epykachu@gmail.com>
2020-03-02 14:32:54 +0000

    #9 - remove epyk name

--- epyk_flask/export/server/epyk_app.py
+++ epyk_flask/export/server/epyk_app.py
@@ -5,17 +5,20 @@ app = Flask(__name__)
 
 
 
-def init_flask_app():
-  for endpoint in epyk_engine.config['endpoints']['blueprints']:
-    mod = importlib.import_module(endpoint)
-    app.register_blueprint(getattr(mod, epyk_engine.config['endpoints']['blueprints'][endpoint]['name']))
+def init_flask_app(engine, excl_endpoints=None):
+  for endpoint in engine.config['endpoints']['blueprints']:
+    if excl_endpoints and endpoint in excl_endpoints:
+      continue
 
-def __init_test():
+    mod = importlib.import_module(endpoint)
+    app.register_blueprint(getattr(mod, engine.config['endpoints']['blueprints'][endpoint]['name']))
+
+def __init_test(engine):
   mod = importlib.import_module('epyk_basic_endpoints')
-  app.register_blueprint(getattr(mod, epyk_engine.config['endpoints']['blueprints']['epyk_basic_endpoints']['name']))
+  app.register_blueprint(getattr(mod, engine.config['endpoints']['blueprints']['basic_endpoints']['name']))
 
 
 if __name__ == '__main__':
-  epyk_engine.init()
-  __init_test()
-  app.run(host=epyk_engine.config['host']['ip'], port=epyk_engine.config['host']['port'], threaded=True)
+  engine = epyk_engine.Engine()
+  __init_test(engine)
+  app.run(host=engine.config['host']['ip'], port=engine.config['host']['port'], threaded=True)
