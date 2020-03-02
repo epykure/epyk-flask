6b5cd97f5052c7d349953e06e5d01d58a952e821
epykachu <epykachu@gmail.com>
2020-03-02 14:31:17 +0000

    #10 - Turn epyk_engine into singleton

--- epyk_flask/epyk_engine.py
+++ epyk_flask/epyk_engine.py
@@ -1,5 +1,4 @@
 import yaml, hashlib, sys, os, importlib
-config = None
 
 class MissingEpykFlaskConfigException(Exception):
   """Exception to be raised when the configuration is missing"""
@@ -20,21 +19,6 @@ class MissingScriptException(Exception):
   """Exception when a script specified couldn't be found in any repository"""
   pass
 
-
-def init(config_path=None):
-  global config
-  if config_path is None:
-    with open('config.yaml', 'r') as f:
-      config = yaml.load(f, Loader=yaml.FullLoader)
-  else:
-    with open(config_path, 'r') as f:
-      config = yaml.load(f, Loader=yaml.FullLoader)
-
-  for repo, repo_attr in config['repos'].items():
-    sys.path.append(repo_attr['path'])
-  for path in config['endpoints']['path']:
-    sys.path.append(path)
-
 def hash_content(file_path):
   with open(file_path, 'rb') as f:
     f_hash = hashlib.blake2b()
@@ -46,72 +30,70 @@ def hash_content(file_path):
       f_hash.update(data)
   return f_hash.hexdigest()
 
-def parse_config(attr, config):
-  """"""
-  attr_len = len(attr)
-  if attr_len != 0:
-    if attr[0] not in config:
-      raise MissingAttrConfigException('Missing attribute %s from configuration' % attr)
+class Engine(object):
 
-    if attr_len > 1:
-      parse_config(attr[1], config[attr[0]])
+  __instance = None
+  config = None
 
-def config_required(*dec_args):
-  """Allows to check specific properties have been set before using a function"""
-  def wrap(func):
-    if config is None:
-      raise MissingEpykFlaskConfigException('Configuration required for endpoint: %s. Set epyk_config from %s' % (func.__name__, __name__))
+  def __new__(cls, config_path=None):
+    if cls.__instance is None:
+      cls.__instance = super(Engine, cls).__new__(cls)
+    return cls.__instance
 
-    for attr in dec_args:
-      parse_config(attr, config)
+  def __init__(self, config_path=None):
+    if self.config is None:
+      if config_path is None:
+        with open('config.yaml', 'r') as f:
+          self.config = yaml.load(f, Loader=yaml.FullLoader)
+      else:
+        with open(config_path, 'r') as f:
+          self.config = yaml.load(f, Loader=yaml.FullLoader)
 
-    def inner(*args, **kwargs):
-      return func(*args, **kwargs)
+      for repo, repo_attr in self.config['repos'].items():
+        sys.path.append(repo_attr['path'])
+      for path in self.config['endpoints']['path']:
+        sys.path.append(path)
 
-    return inner
+  def find_script(self, folder_name, script_name):
+    file_path = os.path.join(self.config['default_repo']['path'], folder_name, script_name)
+    if os.path.exists(file_path):
+      return file_path
 
-  return wrap
+    else:
+      for repo, repo_attr in self.config['repos'].items():
+        file_path = os.path.join(repo_attr['path'], folder_name, script_name)
+        if os.path.exists(file_path):
+          return file_path
 
-def find_script(folder_name, script_name):
-  file_path = os.path.join(config['default_repo']['path'], folder_name, script_name)
-  if os.path.exists(file_path):
-    return file_path
+    return None
 
-  else:
-    for repo, repo_attr in config['repos'].items():
-      file_path = os.path.join(repo_attr['path'], folder_name, script_name)
-      if os.path.exists(file_path):
-        return file_path
+  def run_script(self, folder_name, script_name):
+    """
+    """
+    if not script_name.endswith('.py'):
+      full_name = '%s.py' % script_name
+    else:
+      full_name = script_name
+      script_name = script_name.replace('.py', '')
 
-  return None
+    file_path = self.find_script(folder_name, full_name)
+    if file_path:
+      script_hash = hash_content(file_path)
+      output_name = '%s_%s_%s' % (folder_name, script_name, script_hash)
+      output_path = os.path.join(self.config['html_output'], 'html', '%s.html' % output_name)
+      if os.path.exists(output_path):
+        return output_path
+    else:
+      raise MissingScriptException('Script is missing from the repository configured %s/%s' % (folder_name, script_name))
 
-def run_script(folder_name, script_name):
-  """
-  """
-  if not script_name.endswith('.py'):
-    full_name = '%s.py' % script_name
-  else:
-    full_name = script_name
-    script_name = script_name.replace('.py', '')
+    mod = importlib.import_module('%s.%s' % (folder_name, script_name))
+    rptObj = getattr(mod, 'REPORT_OBJECT', False)
+    if not rptObj:
+      MissingRptObjException('Your report: %s is missing the REPORT_OBJECT attribute which should be an Report Object from %s' % (mod.__name__, Report.__module__))
+    if hasattr(mod, 'FAVICON'):
+      rptObj.logo = mod.FAVICON
+    if getattr(mod, 'CONTROLLED_ACCESS', False):
+      controlLevel = getattr(mod, 'CONTROLLED_LEVEL', 'ENV').upper()
 
-  file_path = find_script(folder_name, full_name)
-  if file_path:
-    script_hash = hash_content(file_path)
-    output_name = '%s_%s_%s' % (folder_name, script_name, script_hash)
-    output_path = os.path.join(config['html_output'], 'html', '%s.html' % output_name)
-    if os.path.exists(output_path):
-      return output_path
-  else:
-    raise MissingScriptException('Script is missing from the repository configured %s/%s' % (folder_name, script_name))
-
-  mod = importlib.import_module('%s.%s' % (folder_name, script_name))
-  rptObj = getattr(mod, 'REPORT_OBJECT', False)
-  if not rptObj:
-    MissingRptObjException('Your report: %s is missing the REPORT_OBJECT attribute which should be an Report Object from %s' % (mod.__name__, Report.__module__))
-  if hasattr(mod, 'FAVICON'):
-    rptObj.logo = mod.FAVICON
-  if getattr(mod, 'CONTROLLED_ACCESS', False):
-    controlLevel = getattr(mod, 'CONTROLLED_LEVEL', 'ENV').upper()
-
-  rptObj.outs.html_file(path=config['html_output'], name=output_name)
-  return os.path.join(config['html_output'], 'html', output_name)
+    rptObj.outs.html_file(path=self.config['html_output'], name=output_name)
+    return os.path.join(self.config['html_output'], 'html', output_name)
